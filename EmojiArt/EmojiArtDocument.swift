d02479c1fb885550a02ce904b0916ef367aa1376
Hannes Richter <scarafleus@gmail.com>
2024-11-25 19:35:34 +0100

    Finished Lesson 10 EmojiArt, EmojiArtDocument, EmojiArtDocumentView, .draggable, .dropDestination

---
+++ EmojiArt/EmojiArtDocument.swift
@@ -0,0 +1,50 @@
+//
+//  EmojiArtDocument.swift
+//  EmojiArt
+//
+//  Created by Hannes Richter on 25.11.24.
+//
+
+import SwiftUI
+
+class EmojiArtDocument: ObservableObject {
+    typealias Emoji = EmojiArt.Emoji
+    
+    @Published private var emojiArt = EmojiArt()
+    
+    init() {
+        emojiArt.addEmoji("🌗", at: .init(x: -200, y: -150), size: 200)
+        emojiArt.addEmoji("🫥", at: .init(x: 200, y: 100), size: 200)
+    }
+    
+    var emojis: [Emoji] {
+        emojiArt.emojis
+    }
+    
+    var background: URL? {
+        emojiArt.background
+    }
+    
+    // MARK: - Intends
+    
+    func setBackground(_ url: URL?) {
+        emojiArt.background = url
+    }
+    
+    func addEmoji(_ emoji: String, at position: Emoji.Position, size: CGFloat) {
+        emojiArt.addEmoji(emoji, at: position, size: Int(size))
+    }
+}
+
+extension EmojiArt.Emoji {
+    var font: Font {
+        Font.system(size: CGFloat(size))
+    }
+}
+
+extension EmojiArt.Emoji.Position {
+    func `in`(_ geometry: GeometryProxy) -> CGPoint {
+        let center = geometry.frame(in: .local).center
+        return CGPoint(x: center.x + CGFloat(x), y: center.y - CGFloat(y))
+    }
+}
---
+++ EmojiArt/EmojiArtDocumentView.swift
@@ -0,0 +1,96 @@
+//
+//  EmojiArtDocumentView.swift
+//  EmojiArt
+//
+//  Created by Hannes Richter on 25.11.24.
+//
+
+import SwiftUI
+
+struct EmojiArtDocumentView: View {
+    typealias Emoji = EmojiArt.Emoji
+    
+    @ObservedObject var document: EmojiArtDocument
+    
+    private let emojis = "🌈✨🦄🌍🛸🌵🍩🐢🎨🦅🌺🚀🧩🐙🎉📸🎯🎶🕶️🍓🎲🍀🔮🏖️🦊🍕🦋🌊🎈👾"
+    private let paletteEmojiSize: CGFloat = 40
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            documentBody
+            ScrollingEmojis(emojis)
+                .font(.system(size: paletteEmojiSize))
+                .padding(.horizontal)
+                .scrollIndicators(.hidden)
+        }
+    }
+    
+    private var documentBody: some View {
+        GeometryReader { geometry in
+            ZStack {
+                Color.white
+                AsyncImage(url: document.background)
+                    .position(Emoji.Position.zero.in(geometry))
+                ForEach(document.emojis) { emoji in
+                    Text(emoji.string)
+                        .font(emoji.font)
+                        .position(emoji.position.in(geometry))
+                }
+            }
+            .dropDestination(for: Sturldata.self) { sturls, location in
+                drop(sturls, at: location, in: geometry)
+            }
+        }
+    }
+    
+    private func drop(_ sturls: [Sturldata], at location: CGPoint, in geometry: GeometryProxy) -> Bool {
+        for sturl in sturls {
+            switch(sturl) {
+            case .url(let url):
+                document.setBackground(url)
+                return true
+            case .string(let emoji):
+                document.addEmoji(
+                    emoji,
+                    at: emojiPosition(at: location, in: geometry),
+                    size: paletteEmojiSize
+                )
+                return true
+            default:
+                break
+            }
+        }
+        return false
+    }
+    
+    private func emojiPosition(at location: CGPoint, in geometry: GeometryProxy) -> Emoji.Position {
+        let center = geometry.frame(in: .local).center
+        return Emoji.Position(
+            x: Int(location.x - center.x),
+            y: Int(-(location.y - center.y))
+        )
+    }
+}
+
+struct ScrollingEmojis: View {
+    let emojis: [String]
+    
+    init(_ emojis: String) {
+        self.emojis = emojis.uniqued.map(String.init)
+    }
+    
+    var body: some View {
+        ScrollView(.horizontal) {
+            HStack {
+                ForEach(emojis, id: \.self) { emoji in
+                    Text(emoji)
+                        .draggable(emoji)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    EmojiArtDocumentView(document: EmojiArtDocument())
+}
